79dd269a4a7edaafed595fef321a27ac0b68a762
NIMITHAC <nimitha0692@gmail.com>
2016-12-15 16:35:42 +0530

    add files

---
+++ 3*3.js
@@ -0,0 +1,9 @@
+$(".row1 .box").click(function(){
+	$(this).toggleClass('green')
+});
+$(".row2 .box").click(function(){
+	$(this).toggleClass('yellow')
+});
+$(".row3 .box").click(function(){
+	$(this).toggleClass('orange')
+});
